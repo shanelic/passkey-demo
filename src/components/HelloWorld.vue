6f66cd2cf29fe5d9b2335728f4ceaab6d72f82bc
Shane @ Red Building Labs <shanelic@rblabs.io>
2023-10-24 15:18:41 +0800

    feat: converter from ArrayBuffer to HexString

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -67,6 +67,11 @@ const fetchChallenge = () => {
   let random = Math.round(Math.random() * 10000);
   return "shanelic" + random;
 };
+const buf2hex = (buffer: ArrayBuffer) => { // buffer is an ArrayBuffer
+  return [...new Uint8Array(buffer)]
+      .map(x => x.toString(16).padStart(2, '0'))
+      .join('');
+}
 </script>
 
 <template>
